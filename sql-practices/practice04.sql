fa34775827ead60a73fe3bba2e1284db3b5a86ff
ryujiung <ryujiung@192.168.0.152>
2023-09-01 15:51:35 +0900

    practice04

--- sql-practices/practice04.sql
+++ sql-practices/practice04.sql
@@ -43,16 +43,58 @@ and d.salary > e.avg_salary;
 
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
+select a.emp_no, concat(a.first_name,' ',a.last_name), d.name, c.dept_name
+from employees a, dept_emp b, departments c, (select b.dept_no as dept_no, concat(a.first_name,' ',a.last_name) as name
+                                    from employees a, dept_manager b
+                                    where a.emp_no = b.emp_no
+                                                and b.to_date = '9999-01-01') d
+where a.emp_no = b.emp_no
+and b.dept_no = c.dept_no
+and c.dept_no = d.dept_no
+and b.to_date='9999-01-01';
 
 
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
+select a.emp_no, concat(a.first_name,' ',a.last_name), b.title, c.salary
+from employees a, titles b, salaries c, dept_emp d, (select a.dept_no as dept_no, avg(b.salary)
+                                          from dept_emp a, salaries b
+                                          where a.emp_no = b.emp_no
+                                          and a.to_date='9999-01-01'
+                                          and b.to_date='9999-01-01'
+                                          group by a.dept_no
+                                                        order by avg(b.salary) desc
+                                          limit 0,1) e
+where a.emp_no = b.emp_no
+and b.emp_no = c.emp_no
+and c.emp_no = d.emp_no
+and d.dept_no = e.dept_no
+and b.to_date='9999-01-01'
+and c.to_date='9999-01-01'
+and d.to_date='9999-01-01'
+order by salary desc;
 
 -- 문제6.
--- 평균 연봉이 가장 높은 부서는? 
+-- 평균 연봉이 가장 높은 부서는?
+select a.dept_no, avg(b.salary)
+from dept_emp a, salaries b
+where a.emp_no = b.emp_no
+and a.to_date='9999-01-01'
+and b.to_date='9999-01-01'
+group by a.dept_no
+order by avg(b.salary) desc
+limit 1; 
 
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
+select a.title , avg(b.salary)
+from titles a, salaries b
+where a.emp_no = b.emp_no
+and a.to_date='9999-01-01'
+and b.to_date='9999-01-01'
+group by a.title
+order by avg(b.salary) desc
+limit 1;
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
